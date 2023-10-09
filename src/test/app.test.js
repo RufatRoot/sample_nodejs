e383d9b56318cdeca50eb0297abed939d50ad422
RufatRoot <rufat.root@gmail.com>
2023-10-09 23:13:59 +0400

    Chatgpt (#2)
    * test chatgpt on pull requests
    
    * test chatgpt on pull requests
    
    * test chatgpt on pull requests
    
    * test chatgpt on pull requests
    
    * test sonarqube on pull requests
    
    * test build
    
    * test build
    
    * test build
    
    * test build
    
    * test build
    
    * test build
    
    * test build
    
    * added docker file
    
    * added npm stop to npm test
    
    * added npm stop to npm test
    
    * added npm stop to npm test
    
    * changed test app
    
    * fixed dockerfile
    
    * test release
    
    * test release 02
    
    * test docker
    
    * test docker
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test reusable workflows
    
    * test release
    
    * test release
    
    * test release
    
    * test release
    
    * test release
    
    * test release
    
    * test release
    
    * test release
    
    * fixed build_test sonar qube job

---
+++ src/test/app.test.js
@@ -0,0 +1,21 @@
+// test-calculator.js
+
+const { add, subtract } = require('../app.js');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Calculator', () => {
+  describe('add', () => {
+    it('should return the sum of two numbers', () => {
+      expect(add(1, 2)).to.equal(3);
+      expect(add(-1, 2)).to.equal(1);
+    });
+  });
+
+  describe('subtract', () => {
+    it('should return the difference between two numbers', () => {
+      expect(subtract(5, 3)).to.equal(2);
+      expect(subtract(2, 2)).to.equal(0);
+    });
+  });
+});
