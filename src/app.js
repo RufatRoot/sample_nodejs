ae4ee37f29c5c96e5091f038db2e17e16b7d6483
Rufat Aliyev <rufat.root@gmail.com>
2023-10-26 23:01:06 +0400

    new test app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-// calculator.js
+// src/app.js
+const express = require('express');
+const app = express();
+const port = 3000;
 
-function add(a, b) {
-    return a + b;
-  }
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
+
+
+
+// // calculator.js
+
+// function add(a, b) {
+//     return a + b;
+//   }
   
-  function subtract(a, b) {
-    return a - b;
-  }
+//   function subtract(a, b) {
+//     return a - b;
+//   }
   
-  module.exports = { add, subtract };
+//   module.exports = { add, subtract };
   
